d992a40d9f116dec3c78d218e2f4e5563809cede
debbyne <kinyadebbyne@gmail.com>
2022-04-20 13:26:57 +0300

    add changes to model

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -47,9 +47,9 @@ class Profile(models.Model):
         if created:
             Profile.objects.create(user=instance)
 
-     @receiver(post_save, sender=User)
-     def save_user_profile(sender, instance, **kwargs):
-        instance.save()
+   #   @receiver(post_save, sender=User)
+   #   def save_user_profile(sender, instance, **kwargs):
+   #      instance.save()
 
      def save_profile(self):
         self.user
